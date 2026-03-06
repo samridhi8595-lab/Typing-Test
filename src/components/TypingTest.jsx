6139e18675c6e1f7aaabb20df699254fdc7361f5
samridhi8595-lab <samridhi8595@gmail.com>
2026-03-06 16:31:54 +0530

    change paragraph after reset

--- src/components/TypingTest.jsx
+++ src/components/TypingTest.jsx
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react'
 
-const paragraph=
-"Technology has become an important part of modern life. Every day people use computers, smartphones, and the internet to communicate, learn new skills, and complete their work more efficiently. The rapid growth of technology has changed the way we live, study, and interact with each other. Students can now access educational resources online, attend virtual classes, and collaborate with classmates from different parts of the world.One of the biggest advantages of technology is that it saves time and increases productivity. Tasks that once required hours of manual work can now be completed in just a few minutes using digital tools. Businesses rely on software to manage data, communicate with clients, and analyze information to make better decisions. Because of these advancements, many industries have become more efficient and competitive. "
+const paragraphs =[
+"Technology has become an important part of modern life. Every day people use computers, smartphones, and the internet to communicate, learn new skills, and complete their work more efficiently. The rapid growth of technology has changed the way we live, study, and interact with each other. Students can now access educational resources online, attend virtual classes, and collaborate with classmates from different parts of the world.One of the biggest advantages of technology is that it saves time and increases productivity. ",
+
+"Learning to type quickly and accurately is an important skill in today's digital world. Many jobs require people to write emails, reports, and documents every day. Practicing typing regularly can help improve both speed and accuracy. By focusing on proper finger placement and consistent practice, anyone can become a faster and more confident typist over time.",
+
+"The internet has completely transformed the way people access information and communicate with others. In the past, finding information required visiting libraries, reading books, or asking experts for guidance. Today, anyone with an internet connection can instantly search for knowledge about almost any topic. Social media platforms allow people to connect with friends and communities from around the world, while video conferencing tools enable remote work and online collaboration. Despite these advantages, it is important for users to develop critical thinking skills when consuming online content.",
+];
 
 const TypingTest = () => {
+  const [paragraph, setParagraph] = useState("");
   const [input,setInput] = useState("");
   const [timeLeft, setTimeLeft] = useState(120);
   const [isStarted, setIsStarted] = useState(false);
   const [isFinished, setIsFinished] = useState(false);
 
+  //Load Random Paragraph
+  useEffect(() => {
+  setParagraph(paragraphs[Math.floor(Math.random() * paragraphs.length)]);
+  }, [])
+  
   //Timer Logic
   useEffect(() => {
-    if (isStarted && timeLeft>0){
-      const timer = setInterval(()=>{
+    let timer;
+
+    if (isStarted && timeLeft > 0){
+       timer = setInterval(()=>{
         setTimeLeft((prev) => prev-1);
       },1000);
-      return ()=> clearInterval(timer);
     }
+     
     if (timeLeft === 0){
-      setIsFinished(true);
       setIsStarted(false);
+      setIsFinished(true);
     }
+     return () => clearInterval(timer);  
+      
     },[isStarted, timeLeft]);
 
     //Start Test
-    const startTest = ()=>{
+    const startTest = () =>{
     if(!isStarted){
       setIsStarted(true);
       setIsFinished(false);
@@ -34,24 +49,29 @@ const TypingTest = () => {
   // Enter key to start
   const handleKeyDown = (e) =>{
     if(e.key === "Enter" && !isStarted){
-      e.preventDedfault();
-      startTest();
+      e.preventDefault();
+      startTest(true);
     }
   };
 
-  //Reset Test
+  //Reset Test + new paragraph
   const resetTest = () =>{
     setInput("");
-    setTimeLeft(60);
+    setTimeLeft(120);
     setIsStarted(false);
     setIsFinished(false);
+
+    const newParagraph = 
+    paragraphs[Math.floor(Math.random() * paragraphs.length)];
+
+    setParagraph(newParagraph);
   };
 
   //Calculations
   const wordsTyped = input.trim().split(/\s+/).filter(Boolean).length;
   const timeSpent = 120-timeLeft;
   const wpm = 
-      timeSpent > 0 ? Math.round((wordsTyped * 120) / timeSpent) : 0;
+      timeSpent > 0 ? Math.round((wordsTyped * 60) / timeSpent) : 0;
   
   const correctChars = input
    .split("")
@@ -74,11 +94,7 @@ const TypingTest = () => {
 
         <textarea className='w-full border rounded p-3 focus-outline-none focus:ring disabled:bg-gray-100'
           rows="8"
-          placeholder={
-            isStarted
-            ? "Start typing..."
-            : "Click Start or press Enter"
-          }
+          placeholder="Press Enter or Start to begin typing..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
@@ -91,22 +107,23 @@ const TypingTest = () => {
           <span>🎯Accuracy: {accuracy}%</span>
          </div>
 
-         <div className='flex gap-3 mt-5'>
+         <div className='flex gap-4 mt-5'>
           <button
           onClick={startTest}
           disabled={isStarted}
-           className='flex-1 bg-blue-950 text-white py-2 rounded hover:bg-blue-800 disabled:opacity-50'
+           className='flex-1 bg-blue-950 text-white py-2 rounded hover:bg-blue-800 cursor-pointer active:scale-95 disabled:opacity-50'
           >
             Start
           </button>
 
           <button
           onClick={resetTest}
-           className='flex-1 bg-blue-950 text-white py-2 rounded hover:bg-blue-800'
+           className='flex-1 bg-blue-950 text-white py-2 rounded hover:bg-blue-800 cursor-pointer active:scale-95'
           >
             Reset
           </button>
          </div>
+
          {!isStarted && !isFinished &&(
           <p className='text-center text-sm text-gray-500 mt-3'>
             Press <b>Enter</b> or click <b>Start</b> to begin
